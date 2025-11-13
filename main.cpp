2141bb8286dc5ff5f48aa7d14168b701486aa89f
iindryt <indreskomantaite7@gmail.com>
2025-11-13 09:50:07 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,14 @@
 #include <vector>
 #include <list>
 #include <string>
-#include <cstdlib> // srand, rand
-#include <ctime>   // time
+#include <fstream>
+#include <ctime>
 
 #include "studentas.h"
-#include "duomenys.h"
 #include "funkcijos.h"
+#include "duomenys.h"
 #include "testavimas.h"
+#include "laikmatis.h"
 
 int main() {
     std::srand(static_cast<unsigned>(std::time(nullptr)));
@@ -33,10 +34,17 @@ int main() {
             char tipas;
             std::cout << "Naudoti konteiner?: vector (v) ar list (l)? ";
             std::cin >> tipas;
-            if (tipas != 'v' && tipas != 'l') { std::cerr << "Klaida!\n"; continue; }
 
-            Studentas s = ivesk(); // turi buti tavo funkcija
-            if (s.nd().empty()) continue;
+            if (tipas != 'v' && tipas != 'l') {
+                std::cerr << "Klaida: netinkamas konteinerio pasirinkimas! Turi buti 'v' arba 'l'.\n";
+                continue;
+            }
+
+            Studentas s = ivesk();
+            if (s.vardas() == "0") continue; // nutraukimas
+
+            // Automatiskai apskaiciuojame rezultatus per Studentas metodus
+            s.apskaiciuotiRezultatus();
 
             if (tipas == 'v') GrupeVector.push_back(s);
             else GrupeList.push_back(s);
@@ -49,33 +57,43 @@ int main() {
             char tipas;
             std::cout << "Naudoti konteineri: vector (v) ar list (l)? ";
             std::cin >> tipas;
-            if (tipas != 'v' && tipas != 'l') { std::cerr << "Klaida!\n"; continue; }
+
+            if (tipas != 'v' && tipas != 'l') {
+                std::cerr << "Klaida: netinkamas konteinerio pasirinkimas! Turi buti 'v' arba 'l'.\n";
+                continue;
+            }
 
             if (tipas == 'v') {
                 auto isFailo = nuskaitytiIsFailoTemplate<std::vector<Studentas>>(failas);
+                if (isFailo.empty()) std::cout << "Tuscias failas. Nera studentu.\n";
                 GrupeVector.insert(GrupeVector.end(), isFailo.begin(), isFailo.end());
             }
             else {
                 auto isFailo = nuskaitytiIsFailoTemplate<std::list<Studentas>>(failas);
+                if (isFailo.empty()) std::cout << "Tuscias failas. Nera studentu.\n";
                 GrupeList.insert(GrupeList.end(), isFailo.begin(), isFailo.end());
             }
         }
         else if (veiksmas == 3) {
-            if (!GrupeVector.empty()) spausdintiRezultatusIrRusiavima(GrupeVector);
-            else if (!GrupeList.empty()) {
-                std::vector<Studentas> laikinas(GrupeList.begin(), GrupeList.end());
-                spausdintiRezultatusIrRusiavima(laikinas);
+            if (!GrupeVector.empty()) {
+                spausdintiRezultatusIrRusiavima(GrupeVector);
             }
-            else std::cout << "Tuscias sarasas.\n";
+            else if (!GrupeList.empty()) {
+                spausdintiRezultatusIrRusiavima(GrupeList); // list palaikomas template funkcijoje
+            }
+            else {
+                std::cout << "Tuscias sarasas. Nera studentu.\n";
+            }
         }
-        else if (veiksmas == 4) break;
+        else if (veiksmas == 4) {
+            break;
+        }
         else if (veiksmas == 5) {
-            generuotiFaila(1000, "studentai_1000.txt", 5);
-            generuotiFaila(10000, "studentai_10000.txt", 5);
-            generuotiFaila(100000, "studentai_100000.txt", 5);
-            generuotiFaila(1000000, "studentai_1000000.txt", 5);
-            generuotiFaila(10000000, "studentai_10000000.txt", 5);
-
+            generuotiFaila(1000, "studentai_1000.txt");
+            generuotiFaila(10000, "studentai_10000.txt");
+            generuotiFaila(100000, "studentai_100000.txt");
+            generuotiFaila(1000000, "studentai_1000000.txt");
+            generuotiFaila(10000000, "studentai_10000000.txt");
         }
         else if (veiksmas == 6) {
             std::string testFailas;
@@ -85,7 +103,9 @@ int main() {
             testuotiKonteineri<std::vector<Studentas>>(testFailas, "std::vector");
             testuotiKonteineri<std::list<Studentas>>(testFailas, "std::list");
         }
-        else std::cerr << "Klaida: netinkamas pasirinkimas!\n";
+        else {
+            std::cerr << "Klaida: netinkamas meniu pasirinkimas!\n";
+        }
     }
 
     return 0;
