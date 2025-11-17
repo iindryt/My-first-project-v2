7f823dacdb6cab57f74732f3cbb4d1dbb1f5ca88
iindryt <indreskomantaite7@gmail.com>
2025-11-17 10:18:50 +0200

    Update studentas.h

--- studentas.h
+++ studentas.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <string>
 #include <vector>
 #include <iostream>
@@ -14,17 +13,21 @@ private:
     float rezMed_;
 
 public:
-    //konstruktorius
+    // Konstruktoriai
     Studentas();
     Studentas(const std::string& vard, const std::string& pav, int egz, const std::vector<int>& nd);
     explicit Studentas(std::istream& is);
 
-    //destruktorius
+    // Kopijavimo konstruktorius (Rule of Three)
+    Studentas(const Studentas& kitas);
+
+    // Kopijavimo priskyrimo operatorius (Rule of Three)
+    Studentas& operator=(const Studentas& kitas);
+
+    // Destruktorius
     ~Studentas();
 
-    
-    // get'eriai
-    
+    // Get'eriai
     std::string vardas() const;
     std::string pavarde() const;
     const std::vector<int>& pazymiai() const;
@@ -32,14 +35,22 @@ public:
     float getVid() const;
     float getMed() const;
 
+    // Skaitymas is srauto
     std::istream& skaitytiStudenta(std::istream& is);
 
-   
+    // Vidurkio ir medianos skaiciavimas
     float skaiciuotiVidurki() const;
     float skaiciuotiMediana() const;
+
+    // Galutinio rezultato apskaiciavimas su svoriais
     void apskaiciuotiRezultatus();
+
+    // Friend operatoriai ivesties/isvesties
+    friend std::istream& operator>>(std::istream& is, Studentas& s);
+    friend std::ostream& operator<<(std::ostream& os, const Studentas& s);
 };
 
+// Pagalbines funkcijos rikiavimui
 bool pagalVarda(const Studentas& a, const Studentas& b);
 bool pagalPavarde(const Studentas& a, const Studentas& b);
 bool pagalGalutini(const Studentas& a, const Studentas& b);
