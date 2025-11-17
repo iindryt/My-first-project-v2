c01106de64ce1cc286c2c59bad69490ea9befdf8
iindryt <indreskomantaite7@gmail.com>
2025-11-17 10:19:06 +0200

    Update studentas.cpp

--- studentas.cpp
+++ studentas.cpp
@@ -1,10 +1,8 @@
 #include "studentas.h"
 #include <algorithm>
 #include <numeric>
-#include <iomanip>
 
-//konstruktoriai
-
+// Konstruktoriai
 Studentas::Studentas()
     : vard_(""), pav_(""), paz_(), egzas_(0), rezVid_(0.0f), rezMed_(0.0f) {
 }
@@ -17,8 +15,26 @@ Studentas::Studentas(std::istream& is) {
     skaitytiStudenta(is);
 }
 
-//destruktorius
+// Kopijavimo konstruktorius
+Studentas::Studentas(const Studentas& kitas)
+    : vard_(kitas.vard_), pav_(kitas.pav_), paz_(kitas.paz_),
+    egzas_(kitas.egzas_), rezVid_(kitas.rezVid_), rezMed_(kitas.rezMed_) {
+}
 
+// Kopijavimo priskyrimo operatorius
+Studentas& Studentas::operator=(const Studentas& kitas) {
+    if (this != &kitas) {
+        vard_ = kitas.vard_;
+        pav_ = kitas.pav_;
+        paz_ = kitas.paz_;
+        egzas_ = kitas.egzas_;
+        rezVid_ = kitas.rezVid_;
+        rezMed_ = kitas.rezMed_;
+    }
+    return *this;
+}
+
+// Destruktorius su clear()
 Studentas::~Studentas() {
     vard_.clear();
     pav_.clear();
@@ -27,8 +43,8 @@ Studentas::~Studentas() {
     rezVid_ = 0.0f;
     rezMed_ = 0.0f;
 }
-//get'eriai
 
+// Get'eriai
 std::string Studentas::vardas() const { return vard_; }
 std::string Studentas::pavarde() const { return pav_; }
 const std::vector<int>& Studentas::pazymiai() const { return paz_; }
@@ -36,18 +52,16 @@ int Studentas::egzaminas() const { return egzas_; }
 float Studentas::getVid() const { return rezVid_; }
 float Studentas::getMed() const { return rezMed_; }
 
-
+// Skaitymas i? srauto
 std::istream& Studentas::skaitytiStudenta(std::istream& is) {
     is >> vard_ >> pav_;
     paz_.clear();
-
     int paz;
-    
     while (is >> paz) {
         paz_.push_back(paz);
+        if (is.peek() == '\n' || is.eof()) break;
     }
 
-    
     if (!paz_.empty()) {
         egzas_ = paz_.back();
         paz_.pop_back();
@@ -56,44 +70,48 @@ std::istream& Studentas::skaitytiStudenta(std::istream& is) {
         egzas_ = 0;
     }
 
-    
-    is.clear();
-
     apskaiciuotiRezultatus();
     return is;
 }
 
-
+// Vidurkio ir medianos skai?iavimas
 float Studentas::skaiciuotiVidurki() const {
     if (paz_.empty()) return 0.0f;
     float suma = std::accumulate(paz_.begin(), paz_.end(), 0);
-    float vid = suma / paz_.size();
-    return 0.4f * vid + 0.6f * egzas_;
+    return suma / paz_.size();
 }
 
 float Studentas::skaiciuotiMediana() const {
     if (paz_.empty()) return 0.0f;
-
     std::vector<int> kopija = paz_;
     std::sort(kopija.begin(), kopija.end());
-    float med;
-
     size_t n = kopija.size();
     if (n % 2 == 0)
-        med = (kopija[n / 2 - 1] + kopija[n / 2]) / 2.0f;
+        return (kopija[n / 2 - 1] + kopija[n / 2]) / 2.0f;
     else
-        med = kopija[n / 2];
-
-    return 0.4f * med + 0.6f * egzas_;
+        return kopija[n / 2];
 }
 
+// Galutini? rezultat? apskai?iavimas su svoriais
 void Studentas::apskaiciuotiRezultatus() {
-    rezVid_ = skaiciuotiVidurki();
-    rezMed_ = skaiciuotiMediana();
+    rezVid_ = 0.4f * skaiciuotiVidurki() + 0.6f * egzas_;
+    rezMed_ = 0.4f * skaiciuotiMediana() + 0.6f * egzas_;
 }
 
+// Ivesties/isvesties operatoriai
+std::istream& operator>>(std::istream& is, Studentas& s) {
+    return s.skaitytiStudenta(is);
+}
 
+std::ostream& operator<<(std::ostream& os, const Studentas& s) {
+    os << s.vard_ << " " << s.pav_ << " Egz: " << s.egzas_
+        << " Vid: " << s.rezVid_ << " Med: " << s.rezMed_
+        << " Pazymiai: ";
+    for (int p : s.paz_) os << p << " ";
+    return os;
+}
 
+// Pagalbines funkcijos rikiavimui
 bool pagalVarda(const Studentas& a, const Studentas& b) {
     return a.vardas() < b.vardas();
 }
