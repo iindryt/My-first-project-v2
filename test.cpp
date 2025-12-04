3644826cc0c7f38eaf70c9ae4d845871b2099b51
iindryt <indreskomantaite7@gmail.com>
2025-12-04 17:35:05 +0200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -10,6 +10,14 @@ TEST(StudentasTest, VidurkioSkaiciavimas) {
 
     EXPECT_FLOAT_EQ(vid, expected);
 }
+TEST(StudentasTest, GalutinioBalSkaiciavimas) {
+    std::vector<int> nd = { 6, 8, 10 };
+    Studentas s("Aiste", "Petrauskaite", 9, nd);
+    s.apskaiciuotiRezultatus();
+    float expected = 0.4f * ((6 + 8 + 10) / 3.0f) + 0.6f * 9;
+    EXPECT_FLOAT_EQ(s.getVid(), expected);
+}
+
 // ==========================
 // Kopijavimo konstruktoriaus testas
 // ==========================
@@ -18,6 +26,8 @@ TEST(StudentasTest, CopyConstructor) {
     Studentas s1("Aiste", "Petrauskaite", 8, nd);
     Studentas s2(s1);
 
+    EXPECT_EQ(s2.vardas(), s1.vardas());
+    EXPECT_EQ(s2.pavarde(), s1.pavarde());
     EXPECT_EQ(s2.pazymiai(), s1.pazymiai());
     EXPECT_EQ(s2.egzaminas(), s1.egzaminas());
     EXPECT_FLOAT_EQ(s2.getVid(), s1.getVid());
@@ -34,6 +44,8 @@ TEST(StudentasTest, AssignmentOperator) {
 
     s2 = s1;
 
+    EXPECT_EQ(s2.vardas(), s1.vardas());
+    EXPECT_EQ(s2.pavarde(), s1.pavarde());
     EXPECT_EQ(s2.pazymiai(), s1.pazymiai());
     EXPECT_EQ(s2.egzaminas(), s1.egzaminas());
     EXPECT_FLOAT_EQ(s2.getVid(), s1.getVid());
