2f75f1cfd9a6342bdaf2c6cf9777143fdccc2a3b
iindryt <indreskomantaite7@gmail.com>
2025-12-04 12:09:11 +0200

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,41 @@
+#include "pch.h"
+#include "studentas.h"
+
+TEST(StudentasTest, VidurkioSkaiciavimas) {
+    std::vector<int> nd = { 6, 8, 10 };
+    Studentas s("Aiste", "Petrauskaite", 9, nd);
+
+    float vid = s.skaiciuotiVidurki();
+    float expected = (6 + 8 + 10) / 3.0f;
+
+    EXPECT_FLOAT_EQ(vid, expected);
+}
+// ==========================
+// Kopijavimo konstruktoriaus testas
+// ==========================
+TEST(StudentasTest, CopyConstructor) {
+    std::vector<int> nd = { 5, 7, 9 };
+    Studentas s1("Aiste", "Petrauskaite", 8, nd);
+    Studentas s2(s1);
+
+    EXPECT_EQ(s2.pazymiai(), s1.pazymiai());
+    EXPECT_EQ(s2.egzaminas(), s1.egzaminas());
+    EXPECT_FLOAT_EQ(s2.getVid(), s1.getVid());
+    EXPECT_FLOAT_EQ(s2.getMed(), s1.getMed());
+}
+// ==========================
+// Priskyrimo operatoriaus testas
+// ==========================
+TEST(StudentasTest, AssignmentOperator) {
+    std::vector<int> nd1 = { 5, 7, 9 };
+    std::vector<int> nd2 = { 2, 4, 6 };
+    Studentas s1("Aiste", "Petrauskaite", 8, nd1);
+    Studentas s2("Jonas", "Jonaitis", 6, nd2);
+
+    s2 = s1;
+
+    EXPECT_EQ(s2.pazymiai(), s1.pazymiai());
+    EXPECT_EQ(s2.egzaminas(), s1.egzaminas());
+    EXPECT_FLOAT_EQ(s2.getVid(), s1.getVid());
+    EXPECT_FLOAT_EQ(s2.getMed(), s1.getMed());
+}
